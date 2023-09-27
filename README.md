f8a9f080bb8ea4b1c2d1ad4862ad51c5ac57ef43
CGQAQ <m.jason.liu@outlook.com>
2023-09-27 16:57:14 +0800

    readme and build script for d8

---
+++ README.md
@@ -0,0 +1,19 @@
+# HOW TO BUILD
+
+## Prerequisites
+- Git
+- Python3
+
+## Build (Non-Windows)
+```bash
+git clone --recurse-submodules https://github.com/CGQAQ/v8.1.git v8.1
+cd v8.1
+python3 build-v8.py
+```
+
+## Build (Windows)
+```bash
+git clone --recurse-submodules https://github.com/CGQAQ/v8.1.git v8.1
+cd v8.1
+python build-v8.py
+```
--- build-v8.py
+++ build-v8.py
@@ -7,6 +7,9 @@ import subprocess
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'depot_tools'))
 os.environ["PATH"] = (os.path.join(os.path.dirname(__file__), 'depot_tools')) + os.pathsep + os.environ["PATH"]
 
+# disable depot_tools win_toolchain fetching
+os.environ["DEPOT_TOOLS_WIN_TOOLCHAIN"] = "0"
+
 subprocess.check_call(['vpython3.bat', './depot_tools/update_depot_tools_toggle.py', '--disable'])
 
 subprocess.check_call(['vpython3.bat', './depot_tools/gclient.py', 'config', 
@@ -22,4 +25,9 @@ subprocess.check_call(['vpython3.bat', './depot_tools/gclient.py', 'config',
 ])
 subprocess.check_call(['gclient.bat', 'sync', '--with_branch_heads'])
 
-subprocess.check_call(['gn.bat', 'gen', 'out/Default', 'v8'])
+___workdir = os.getcwd()
+os.chdir("v8")
+subprocess.check_call(['gn.bat', 'gen', '../out/Default', '--args=is_debug=false'])
+os.chdir(___workdir)
+
+subprocess.check_call(['autoninja.bat', '-C', 'out/Default', 'd8'])
